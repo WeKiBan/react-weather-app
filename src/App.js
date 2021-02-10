5a56a6cb92cb9431f9096ab8997ec1bb8f3385c1
Wekiban <wes.banyard@googlemail.com>
2021-02-10 16:25:48 +0100

    added stling and structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './app.css';
+import { format } from 'date-fns';
+
+const api = {
+  key: '2e44188ae7e877bf4fe23dac83d52bae',
+  base: 'https://api.openweathermap.org/data/2.5',
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+        <div className="location-box">
+          <div className="location">New York City, US</div>
+          <div className="date">{format(new Date(), 'EEE do MMM yyyy')}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">15&deg;</div>
+          <div className="weather">Sunny</div>
+        </div>
+      </main>
     </div>
   );
 }
